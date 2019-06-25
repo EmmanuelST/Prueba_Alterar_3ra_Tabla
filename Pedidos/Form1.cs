424776263fbda52fe06ee615185e5a91e2c6730e
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-06-24 20:47:33 -0400

    Agregando todo (Ahora si)

---
+++ Pedidos/Form1.cs
@@ -0,0 +1,228 @@
+﻿using Entidades.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pedidos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
+namespace Entidades
+{
+    public class Pedidos
+    {
+        [Key]
+        public int PedidoId { get; set; }
+        public DateTime Fecha { get; set; }
+        public double Monto { get; set; }
+        [ForeignKey("ArticuloId")]
+        public virtual List<PedidosDetalle> Detalle { get; set; }
+
+        public Pedidos()
+        {
+            this.Detalle = new List<PedidosDetalle>();
+        }
+        public void AgregarDetalle(PedidosDetalle pedido)
+        {
+            this.Detalle.Add(pedido);
+        }
+    }
+
+    public class PedidosDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int PedidoId { get; set; }
+        public int ArticuloId { get; set; }
+        public decimal Cantidad { get; set; }
+        public double Precio { get; set; }
+
+        public PedidosDetalle()
+        {
+            Id = 0;
+            PedidoId = 0;
+            ArticuloId = 0;
+            Cantidad = 0;
+            Precio = 0;
+        }
+    }
+
+    public class Articulos
+    {
+        [Key]
+        public int ArticuloId { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Existencia { get; set; }
+        public decimal Precio { get; set; }
+
+        public Articulos()
+        {
+            ArticuloId = 0;
+            Descripcion = string.Empty;
+            Existencia = 0;
+            Precio = 0;
+        }
+    } 
+
+    namespace DAL
+    {
+        public class Contexto : DbContext
+        {
+            public DbSet<Pedidos>Pedido { get; set; }
+            public DbSet<Articulos>Articulo { get; set; }
+
+            public Contexto() : base(@"Data Source=.\SqlExpress;Initial Catalog = RepasoDb; Integrated Security = True")
+            {
+
+            }
+        }
+    }
+
+    namespace BLL
+    {
+        public interface IRepository<T> where T : class
+        {
+            List<T> GetList(Expression<Func<T, bool>> Expression);
+            T Buscar(int id);
+            bool Guardar(T entity);
+            bool Modificar(T entity);
+            bool Elimimar(int id);
+        }
+
+        public class RepositorioBase<T> : IDisposable, IRepository<T> where T : class
+        {
+
+            internal Contexto db;
+
+            public RepositorioBase()
+            {
+                db = new Contexto();
+            }
+            public virtual bool Guardar(T entity)
+            {
+                bool paso = false;
+
+                try
+                {
+                    if (db.Set<T>().Add(entity) != null)
+                        paso = db.SaveChanges() > 0;
+
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+
+                return paso;
+            }
+
+            public virtual bool Modificar(T entity)
+            {
+                bool paso = false;
+
+                try
+                {
+
+                    db.Entry(entity).State = EntityState.Modified;
+                    paso = db.SaveChanges() > 0;
+
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+
+                return paso;
+            }
+            public T Buscar(int id)
+            {
+                T entity;
+
+                try
+                {
+                    entity = db.Set<T>().Find(id);
+
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+
+                return entity;
+            }
+            public List<T> GetList(Expression<Func<T, bool>> expression)
+            {
+                List<T> lista;
+
+                try
+                {
+                    lista = db.Set<T>().Where(expression).ToList();
+
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+
+                return lista;
+            }
+            
+            public void Dispose()
+            {
+                db.Dispose();
+            }
+
+            public bool Elimimar(int id)
+            {
+                bool paso = false;
+                T entity;
+
+                try
+                {
+                    entity = db.Set<T>().Find(id);
+                    db.Entry(entity).State = EntityState.Deleted;
+                    paso = db.SaveChanges() > 0;
+
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+
+                return paso;
+            }
+        }
+
+        public class PedidoRepositorio : RepositorioBase<Pedidos>
+        {
+
+            
+        }
+
+    }
+
+
+
+}
---
+++ PedidosTests/RepositorioBaseTests.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pedidos;
+
+namespace Entidades.BLL.Tests
+{
+    [TestClass()]
+    public class RepositorioBaseTests
+    {
+        [TestMethod()]
+        public void GuardarArtiuloTest()
+        {
+            RepositorioBase<Articulos> db = new RepositorioBase<Articulos>();
+
+            Articulos entity = new Articulos()
+            {
+                ArticuloId = 0,
+                Descripcion = "Prueba1",
+                Existencia = 5,
+                Precio = 500
+            };
+
+            Assert.IsTrue(db.Guardar(entity));
+        }
+
+        [TestMethod()]
+        public void GuardarPedidosTest()
+        {
+            RepositorioBase<Pedidos> db = new RepositorioBase<Pedidos>();
+            List<PedidosDetalle> Detalle = new List<PedidosDetalle>();
+
+            Detalle.Add(new PedidosDetalle() {
+                Id = 0,
+                ArticuloId = 1,
+                PedidoId = 0,
+                Cantidad = 5,
+                Precio = 500
+            });
+
+            Detalle.Add(new PedidosDetalle()
+            {
+                Id = 0,
+                ArticuloId = 2,
+                PedidoId = 0,
+                Cantidad = 4,
+                Precio = 400
+            });
+
+            Pedidos entity = new Pedidos()
+            {
+                PedidoId = 0,
+                Fecha = DateTime.Now,
+                Monto = 500,
+                Detalle = Detalle
+                
+            };
+
+            Assert.IsTrue(db.Guardar(entity));
+        }
+
+        [TestMethod()]
+        public void BuscarArtiuloTest()
+        {
+            RepositorioBase<Articulos> db = new RepositorioBase<Articulos>();
+
+            Articulos entity = new Articulos()
+            {
+                ArticuloId = 1,
+                Descripcion = "Prueba1",
+                Existencia = 5,
+                Precio = 500
+            };
+
+            Assert.IsNotNull(db.Buscar(entity.ArticuloId));
+        }
+
+        [TestMethod()]
+        public void BuscarPedidoTest()
+        {
+            RepositorioBase<Pedidos> db = new RepositorioBase<Pedidos>();
+
+            Pedidos entity = new Pedidos()
+            {
+                PedidoId = 1,
+                Fecha = DateTime.Now,
+                Monto = 500
+            };
+
+            Assert.IsNotNull(db.Buscar(entity.PedidoId));
+        }
+
+        [TestMethod()]
+        public void GetListArticuloTest()
+        {
+            RepositorioBase<Articulos> db = new RepositorioBase<Articulos>();
+
+            
+
+            Assert.IsNotNull(db.GetList(A => true));
+        }
+
+        [TestMethod()]
+        public void GetListPedidoTest()
+        {
+            RepositorioBase<Pedidos> db = new RepositorioBase<Pedidos>();
+
+
+
+            Assert.IsNotNull(db.GetList(A => true));
+        }
+
+        [TestMethod()]
+        public void ModificarArticuloTest()
+        {
+            RepositorioBase<Articulos> db = new RepositorioBase<Articulos>();
+
+            Articulos entity = new Articulos()
+            {
+                ArticuloId = 1,
+                Descripcion = "Prueba2",
+                Existencia = 5,
+                Precio = 500
+            };
+            Assert.IsNotNull(db.Modificar(entity));
+        }
+
+        [TestMethod()]
+        public void ModificarPedidoTest()
+        {
+            RepositorioBase<Pedidos> db = new RepositorioBase<Pedidos>();
+
+            Pedidos entity = new Pedidos()
+            {
+                PedidoId = 1,
+                Fecha = DateTime.Now,
+                Monto = 500
+
+            };
+
+
+            Assert.IsNotNull(db.Modificar(entity));
+        }
+
+        [TestMethod()]
+        public void EliminarPedidoTest()
+        {
+            RepositorioBase<Pedidos> db = new RepositorioBase<Pedidos>();
+
+
+            Assert.IsNotNull(db.Elimimar(1));
+        }
+
+        [TestMethod()]
+        public void EliminarArticuloTest()
+        {
+            RepositorioBase<Articulos> db = new RepositorioBase<Articulos>();
+
+
+            Assert.IsNotNull(db.Elimimar(1));
+        }
+    }
+}
